d09c08d4c1f870ae95c2ede3b7c81267f8dbf3ef
dxzhang <dxzhang@ustc.edu>
2011-08-12 09:23:09 +0800

    add Lab1_FPGA

---
+++ Lab1_FPGA/Lab1_tb.v
@@ -0,0 +1,22 @@
+`timescale 1ns/100ps
+
+module Lab1_tb;
+reg in1;
+reg in2;
+wire out1;
+wire out2;
+
+Lab1 Lab1_inst(in1,in2,out1,out2);
+
+initial begin
+	in1=1'b1;
+	in2=1'b1;
+	#10 in1=1'b0;
+			in2=1'b1;
+	#10 in1=1'b1;
+			in2=1'b0;
+	#10 in1=1'b0;
+			in2=1'b0;
+	#10	$stop;	
+end
+endmodule
