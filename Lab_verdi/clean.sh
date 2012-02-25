a1a88318ceb1c0a44b76ed748fc6d92ea7906273
dxzhang <dxzhang@ust.edu>
2012-02-25 11:05:50 +0800

    add Lab_verdi

---
+++ Lab_verdi/clean.sh
@@ -0,0 +1,13 @@
+rm -rf transcript
+rm -rf work
+rm -rf mywork
+rm -rf *.wlf
+rm -rf modelsim.ini
+rm -rf *.log
+rm -rf verdiLog
+rm -rf novas.rc
+rm -rf *.vcd 
+rm -rf *.fsdb
+rm -rf *.ucdb
+rm -rf *.rpt
+rm -rf covhtmlreport
---
+++ Lab_verdi/test_alu.v
@@ -0,0 +1,47 @@
+`include "timescale.v"
+module test_alu ;
+
+reg [11:00] assembly_lang [10:00];
+reg [11:00] temp_assmb;
+integer i;
+reg  [03:00] opcode,in1,in2;
+wire [03:00] result;
+                
+
+initial
+   begin
+     $readmemh ("assmb.bin",assembly_lang);
+     for ( i=0; i <= 10;i=i+1)
+     begin
+        temp_assmb = assembly_lang[i];
+        if(temp_assmb === 12'hx)
+          $finish;
+        opcode = temp_assmb[11:08];
+        in1    = temp_assmb[07:04];
+        in2    = temp_assmb[03:00];
+      #1;
+     end
+
+  end
+
+
+alu alu_inst (
+          .result              (result              ),
+          .in1                 (in1                 ),
+          .in2                 (in2                 ),
+          .opcode              (opcode              )
+          );
+
+initial begin
+     $monitor ( "Opcode =%d in1 =%d in2 =%d result =%d",opcode,in1,in2,result);
+end     
+
+`ifdef  DUMP_FSDB
+initial begin
+        $fsdbDumpfile("test_alu.fsdb");
+        $fsdbDumpvars;
+end
+`endif
+
+
+endmodule
